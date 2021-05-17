0633e3f2a9a71d28137164f0b07f568b254dff4e
KrzysztofPajak <krzysiek@grandnode.com>
2021-05-17 19:39:16 +0200

    Add repository extensions

---
+++ src/Core/Grand.Domain/Data/RepositoryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grand.Domain.Data
+{
+    public static class RepositoryExtensions
+    {
+        public static Task<List<T>> ToListAsync2<T>(this IQueryable<T> query)
+        {
+            return ((IMongoQueryable<T>)query).ToListAsync();
+        }
+
+        public static Task<List<T>> ToListAsync2<T>(this IMongoQueryable<T> query)
+        {
+            return query.ToListAsync();
+        }
+
+        public static Task<T> FirstOrDefaultAsync2<T>(this IMongoQueryable<T> query)
+        {
+            return query.FirstOrDefaultAsync();
+        }
+
+        public static Task<T> FirstOrDefaultAsync2<T>(this IOrderedQueryable<T> query)
+        {
+            return ((IMongoQueryable<T>)query).FirstOrDefaultAsync();
+        }
+    }
+}
