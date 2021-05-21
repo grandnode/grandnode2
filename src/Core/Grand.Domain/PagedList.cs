e5c9011da90fe596416029db71c577840ca731ef
KrzysztofPajak <krzysiek@grandnode.com>
2021-05-21 14:01:15 +0200

    Remove unused method - page list

--- src/Core/Grand.Domain/PagedList.cs
+++ src/Core/Grand.Domain/PagedList.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Grand.Domain.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Grand.Domain
 {
@@ -46,6 +48,34 @@ namespace Grand.Domain
         public PagedList(IEnumerable<T> source, int pageIndex, int pageSize, int totalCount)
         {
             Initialize(source, pageIndex, pageSize, totalCount);
+        }
+        private async Task InitializeAsync(IQueryable<T> source, int pageIndex, int pageSize, int? totalCount = null)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (pageSize <= 0)
+                pageSize = 1;
+
+            TotalCount = totalCount ?? await source.CountAsync2();
+            source = totalCount == null ? source.Skip(pageIndex * pageSize).Take(pageSize) : source;
+            AddRange(source);
+
+            if (pageSize > 0)
+            {
+                TotalPages = TotalCount / pageSize;
+                if (TotalCount % pageSize > 0)
+                    TotalPages++;
+            }
+
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+        }
+
+        public static async Task<PagedList<T>> Create(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var pagelist = new PagedList<T>();
+            await pagelist.InitializeAsync(source, pageIndex, pageSize);
+            return pagelist;
         }
 
         public int PageIndex { get; private set; }
