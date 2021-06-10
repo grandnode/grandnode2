b249fecf02fa44a6fd683a44a676ccbdc6de5687
KrzysztofPajak <krzysiek@grandnode.com>
2021-06-10 11:56:18 +0200

    Update firebasejs

--- src/Web/Grand.Web/wwwroot/firebase-messaging-sw.js
+++ src/Web/Grand.Web/wwwroot/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/7.21.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.21.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.5/firebase-messaging.js');
 
 var config = {
 apiKey: "",
