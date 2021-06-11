ec18a41b5919a30b04b94972506c306387dbc96b
KrzysztofPajak <krzysiek@grandnode.com>
2021-06-11 14:26:38 +0200

    Improve components path - allow to use views per themes in the plugin

---
+++ src/Web/Grand.Web.Common/Components/ViewComponentExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Grand.Web.Common.Themes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Grand.Web.Common.Components
+{
+    public static class ViewComponentExtensions
+    {
+        public static string GetViewPath(this ViewComponent viewComponent, string viewName = "Default")
+        {
+            var themeContext = viewComponent.HttpContext.RequestServices.GetService<IThemeContext>();
+            var theme = themeContext.WorkingThemeName;
+
+            var viewPath = $"Views/Shared/Components/{viewComponent.ViewComponentContext.ViewComponentDescriptor.ShortName}/{viewName}.cshtml";
+            var themeViewPath = $"/Themes/{theme}{viewPath}";
+            var viewEngine = viewComponent.ViewContext.HttpContext.RequestServices.GetRequiredService<ICompositeViewEngine>();
+            var result = viewEngine.GetView("", themeViewPath, isMainPage: false);
+            if (result.Success)
+            {
+                viewPath = themeViewPath;
+            }
+
+            return viewPath;
+        }
+    }
+}
