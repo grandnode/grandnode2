bd324cf49f2ab08e0cd3a72d09769f670577841c
KrzysztofPajak <krzysiek@grandnode.com>
2021-08-30 08:53:48 +0200

    Picture thumbs can not be deleted from local storage (#101)

--- src/Business/Grand.Business.Storage/Services/PictureService.cs
+++ src/Business/Grand.Business.Storage/Services/PictureService.cs
@@ -148,10 +148,13 @@ namespace Grand.Business.Storage.Services
                 {
                     try
                     {
-                        if (!File.Exists(currentFileName))
+                        if (File.Exists(currentFileName))
                             File.Delete(currentFileName);
                     }
-                    catch { }
+                    catch (Exception ex)
+                    {
+                        _logger.InsertLog(Domain.Logging.LogLevel.Error, ex.Message);
+                    }
                 }
             }
             return Task.CompletedTask;
