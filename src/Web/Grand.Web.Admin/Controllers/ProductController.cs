dbd4720e930901343a6b5004c9eb736e981072d0
DanielŻuławski <dzulawski@unit-soft.pl>
2021-09-21 16:01:00 +0200

    SimilarProductAddPopup - magnific popup layout

--- src/Web/Grand.Web.Admin/Controllers/ProductController.cs
+++ src/Web/Grand.Web.Admin/Controllers/ProductController.cs
@@ -753,14 +753,15 @@ namespace Grand.Web.Admin.Controllers
                 //a vendor should have access only to his products
                 model.IsLoggedInAsVendor = _workContext.CurrentVendor != null;
                 ViewBag.RefreshPage = true;
+                return Content("");
             }
             else
             {
                 Error(ModelState);
                 model = await _productViewModelService.PrepareSimilarProductModel();
                 model.ProductId = model.ProductId;
+                return View(model);
             }
-            return View(model);
         }
 
         #endregion
