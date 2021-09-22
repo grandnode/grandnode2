c987e99dec13ade9dd5a559f61695a4d54f6abaa
DanielŻuławski <dzulawski@unit-soft.pl>
2021-09-22 14:29:55 +0200

    AssociatedProductAddPopup - magnific popup layout

--- src/Web/Grand.Web.Admin/Controllers/ProductController.cs
+++ src/Web/Grand.Web.Admin/Controllers/ProductController.cs
@@ -1170,14 +1170,15 @@ namespace Grand.Web.Admin.Controllers
                 //a vendor should have access only to his products
                 model.IsLoggedInAsVendor = _workContext.CurrentVendor != null;
                 ViewBag.RefreshPage = true;
+                return Content("");
             }
             else
             {
                 Error(ModelState);
                 model = await _productViewModelService.PrepareAssociatedProductModel();
                 model.ProductId = model.ProductId;
+                return View(model);
             }
-            return View(model);
         }
 
         #endregion
