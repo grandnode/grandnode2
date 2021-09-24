791d4143703474878a13a977cda110fbcc036294
DanielŻuławski <dzulawski@unit-soft.pl>
2021-09-24 16:03:34 +0200

    PicturePopup brand - magnific popup layout

--- src/Web/Grand.Web.Admin/Controllers/BrandController.cs
+++ src/Web/Grand.Web.Admin/Controllers/BrandController.cs
@@ -326,7 +326,7 @@ namespace Grand.Web.Admin.Controllers
                 await _pictureViewModelService.UpdatePicture(model);
 
                 ViewBag.RefreshPage = true;
-                return View("PicturePopup", model);
+                return Content("");
             }
 
             Error(ModelState);
