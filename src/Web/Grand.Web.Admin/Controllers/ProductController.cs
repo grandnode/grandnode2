fd5456f2be7c00de004237d7d0272eb06f002fd8
DanielŻuławski <dzulawski@unit-soft.pl>
2021-09-24 10:50:30 +0200

    BundleProductAddPopup - magnific popup layout

--- src/Web/Grand.Web.Admin/Controllers/ProductController.cs
+++ src/Web/Grand.Web.Admin/Controllers/ProductController.cs
@@ -858,14 +858,15 @@ namespace Grand.Web.Admin.Controllers
                 //a vendor should have access only to his products
                 model.IsLoggedInAsVendor = _workContext.CurrentVendor != null;
                 ViewBag.RefreshPage = true;
+                return Content("");
             }
             else
             {
                 Error(ModelState);
                 model = await _productViewModelService.PrepareBundleProductModel();
                 model.ProductId = model.ProductId;
+                return View(model);
             }
-            return View(model);
         }
 
         #endregion
