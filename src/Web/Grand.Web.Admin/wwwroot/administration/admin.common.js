23849ad43970472e58e8823dca6fa089d8c1c274
KrzysztofPajak <krzysiek@grandnode.com>
2021-09-28 18:12:08 +0200

    Remove unused javascript functions

--- src/Web/Grand.Web.Admin/wwwroot/administration/admin.common.js
+++ src/Web/Grand.Web.Admin/wwwroot/administration/admin.common.js
@@ -2,15 +2,6 @@ function setLocation(url) {
     window.location.href = url;
 }
 
-function OpenWindow(query, w, h, scroll) {
-    var l = (screen.width - w) / 2;
-    var t = (screen.height - h) / 2;
-
-    winprops = 'resizable=1, height=' + h + ',width=' + w + ',top=' + t + ',left=' + l + 'w';
-    if (scroll) winprops += ',scrollbars=1';
-    var f = window.open(query, "_blank", winprops);
-}
-
 function showThrobber(message) {
     $('.throbber-header').html(message);
     window.setTimeout(function () {
