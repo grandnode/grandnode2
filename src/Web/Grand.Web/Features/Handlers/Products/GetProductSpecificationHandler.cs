3966c28705027017fdc3868a9516f82ed86fd3ec
KrzysztofPajak <krzysiek@grandnode.com>
2021-10-12 10:40:25 +0200

    GetProductSpecificationHandler - prevent for NullReferenceException

--- src/Web/Grand.Web/Features/Handlers/Products/GetProductSpecificationHandler.cs
+++ src/Web/Grand.Web/Features/Handlers/Products/GetProductSpecificationHandler.cs
@@ -1,6 +1,6 @@
-﻿using Grand.Domain.Catalog;
-using Grand.Business.Catalog.Interfaces.Categories;
-using Grand.Business.Common.Interfaces.Localization;
+﻿using Grand.Business.Catalog.Interfaces.Products;
+using Grand.Business.Common.Extensions;
+using Grand.Domain.Catalog;
 using Grand.Web.Features.Models.Products;
 using Grand.Web.Models.Catalog;
 using MediatR;
@@ -10,8 +10,6 @@ using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
-using Grand.Business.Catalog.Interfaces.Products;
-using Grand.Business.Common.Extensions;
 
 namespace Grand.Web.Features.Handlers.Products
 {
@@ -32,18 +30,21 @@ namespace Grand.Web.Features.Handlers.Products
             var spa = new List<ProductSpecificationModel>();
             foreach (var item in request.Product.ProductSpecificationAttributes.Where(x => x.ShowOnProductPage).OrderBy(x => x.DisplayOrder))
             {
-                var specificationAttribute = await _specificationAttributeService.GetSpecificationAttributeById(item.SpecificationAttributeId);
                 var m = new ProductSpecificationModel {
                     SpecificationAttributeId = item.SpecificationAttributeId,
-                    SpecificationAttributeName = specificationAttribute.GetTranslation(x => x.Name, request.Language.Id),
-                    ColorSquaresRgb = specificationAttribute.SpecificationAttributeOptions.Where(x => x.Id == item.SpecificationAttributeOptionId).FirstOrDefault() != null ? specificationAttribute.SpecificationAttributeOptions.Where(x => x.Id == item.SpecificationAttributeOptionId).FirstOrDefault().ColorSquaresRgb : "",
-                    UserFields = specificationAttribute.UserFields,
                 };
 
                 switch (item.AttributeTypeId)
                 {
                     case SpecificationAttributeType.Option:
-                        m.ValueRaw = WebUtility.HtmlEncode(specificationAttribute.SpecificationAttributeOptions.Where(x => x.Id == item.SpecificationAttributeOptionId).FirstOrDefault().GetTranslation(x => x.Name, request.Language.Id));
+                        var specificationAttribute = await _specificationAttributeService.GetSpecificationAttributeById(item.SpecificationAttributeId);
+                        if (specificationAttribute != null)
+                        {
+                            m.ValueRaw = WebUtility.HtmlEncode(specificationAttribute.SpecificationAttributeOptions.Where(x => x.Id == item.SpecificationAttributeOptionId).FirstOrDefault().GetTranslation(x => x.Name, request.Language.Id));
+                            m.SpecificationAttributeName = specificationAttribute.GetTranslation(x => x.Name, request.Language.Id);
+                            m.ColorSquaresRgb = specificationAttribute.SpecificationAttributeOptions.Where(x => x.Id == item.SpecificationAttributeOptionId).FirstOrDefault() != null ? specificationAttribute.SpecificationAttributeOptions.Where(x => x.Id == item.SpecificationAttributeOptionId).FirstOrDefault().ColorSquaresRgb : "";
+                            m.UserFields = specificationAttribute.UserFields;
+                        }
                         break;
                     case SpecificationAttributeType.CustomText:
                         m.ValueRaw = WebUtility.HtmlEncode(item.CustomValue);
