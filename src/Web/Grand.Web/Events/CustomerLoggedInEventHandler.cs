aeb36d8764b058ae031332f65d5defe2029f1f62
KrzysztofPajak <krzysiek@grandnode.com>
2021-10-22 10:32:34 +0200

    Minor changes with event handlers: CustomerLoggedInEventHandler/CustomerLoggedOutEventHandler

--- src/Web/Grand.Web/Events/CustomerLoggedInEventHandler.cs
+++ src/Web/Grand.Web/Events/CustomerLoggedInEventHandler.cs
@@ -2,28 +2,35 @@
 using Grand.Business.Common.Interfaces.Logging;
 using Grand.Business.Customers.Events;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Grand.Business.Marketing.Events
+namespace Grand.Web.Events
 {
     public class CustomerLoggedInEventHandler : INotificationHandler<CustomerLoggedInEvent>
     {
         private readonly ICustomerActivityService _customerActivityService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly ITranslationService _translationService;
 
         public CustomerLoggedInEventHandler(
             ICustomerActivityService customerActivityService,
+            IHttpContextAccessor httpContextAccessor,
             ITranslationService translationService)
         {
             _customerActivityService = customerActivityService;
+            _httpContextAccessor = httpContextAccessor;
             _translationService = translationService;
         }
 
         public Task Handle(CustomerLoggedInEvent notification, CancellationToken cancellationToken)
         {
             //activity log
-            _ = _customerActivityService.InsertActivity("PublicStore.Login", notification.Customer.Id, notification.Customer, "", _translationService.GetResource("ActivityLog.PublicStore.Login"));
+            _ = _customerActivityService.InsertActivity("PublicStore.Login", 
+                notification.Customer.Id, notification.Customer,
+                _httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress?.ToString(),
+                _translationService.GetResource("ActivityLog.PublicStore.Login"));
             return Task.CompletedTask;
         }
     }
