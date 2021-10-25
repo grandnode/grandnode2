bd4d7d3600ff07127a3a0dc7f982f5c7cc796865
KrzysztofPajak <krzysiek@grandnode.com>
2021-10-25 12:09:36 +0200

    Add Voice Navigation components

---
+++ src/Web/Grand.Web/Components/VoiceNavigation.cs
@@ -0,0 +1,25 @@
+﻿using Grand.Domain.Stores;
+using Grand.Web.Common.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grand.Web.Components
+{
+    public class VoiceNavigationViewComponent : BaseViewComponent
+    {
+        private readonly StoreInformationSettings _storeInformationSettings;
+
+        public VoiceNavigationViewComponent(
+            StoreInformationSettings storeInformationSettings)
+        {
+            _storeInformationSettings = storeInformationSettings;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            if (!_storeInformationSettings.VoiceNavigation)
+                return Content("");
+
+            return View();
+        }
+    }
+}
