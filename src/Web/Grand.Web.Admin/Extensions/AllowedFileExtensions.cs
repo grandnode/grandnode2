b67040bdeb90fbbb8b2c19f34b78e891e9b56d70
KrzysztofPajak <krzysiek@grandnode.com>
2021-11-09 08:16:49 +0100

    Double code removal - GetAllowedFileTypes

---
+++ src/Web/Grand.Web.Admin/Extensions/AllowedFileExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Grand.Web.Admin.Extensions
+{
+    public static class FileExtensions
+    {
+        public static IList<string> GetAllowedMediaFileTypes(string allowedFileTypes)
+        {
+            if (string.IsNullOrEmpty(allowedFileTypes))
+                return new List<string> { ".gif", ".jpg", ".jpeg", ".png", ".bmp", ".webp" };
+            else
+                return allowedFileTypes.Split(',').Select(x => x.Trim().ToLowerInvariant()).ToList();
+        }
+    }
+}
