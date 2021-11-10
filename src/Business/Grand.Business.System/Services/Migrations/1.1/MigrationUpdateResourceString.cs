be2604fc98edb69149507122cc3fffa15bdb5dfe
KrzysztofPajak <krzysiek@grandnode.com>
2021-11-10 15:22:59 +0100

    Add migration process to create a new resource string for the next version 1.1.0

---
+++ src/Business/Grand.Business.System/Services/Migrations/1.1/MigrationUpdateResourceString.cs
@@ -0,0 +1,61 @@
+﻿using Grand.Business.Common.Interfaces.Localization;
+using Grand.Business.Common.Interfaces.Logging;
+using Grand.Domain.Data;
+using Grand.Domain.Localization;
+using Grand.Infrastructure.Migrations;
+using Grand.SharedKernel.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Grand.Business.System.Services.Migrations._1._1
+{
+    public class MigrationUpdateResourceString : IMigration
+    {
+
+        public int Priority => 0;
+
+        public DbVersion Version => new(1, 1);
+
+        public Guid Identity => new("21B022B6-FDA7-4574-A395-613A3FD80F5D");
+
+        public string Name => "Update resource string for english language";
+
+        /// <summary>
+        /// Upgrade process
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
+        public bool UpgradeProcess(IDatabaseContext database, IServiceProvider serviceProvider)
+        {
+            var langRepository = serviceProvider.GetRequiredService<IRepository<Language>>();
+            var logService = serviceProvider.GetRequiredService<ILogger>();
+            var translationService = serviceProvider.GetRequiredService<ITranslationService>();
+
+            try
+            {
+                var language = langRepository.Table.FirstOrDefault(l => l.Name == "English");
+
+                if (language == null)
+                    language = langRepository.Table.FirstOrDefault();
+
+                var filePath = CommonPath.MapPath("App_Data/Resources/Upgrade/en_110.xml");
+                var localesXml = File.ReadAllText(filePath);
+                translationService.ImportResourcesFromXmlInstall(language, localesXml).GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                logService.InsertLog(Domain.Logging.LogLevel.Error, "UpgradeProcess - MigrationUpdateResourceString", ex.Message).GetAwaiter().GetResult();
+            }
+            return true;
+        }
+
+        private class SettingsMedia
+        {
+            public bool StoreInDb { get; set; }
+        }
+
+    }
+}
