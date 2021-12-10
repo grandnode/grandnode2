8e48217da1bbb7f8a5488ae08355c8a20af47ec2
KrzysztofPajak <krzysiek@grandnode.com>
2021-12-10 10:28:19 +0100

    API - ProductAttributeCombinationDto - add missing field

--- src/API/Grand.Api/DTOs/Catalog/ProductAttributeCombinationDto.cs
+++ src/API/Grand.Api/DTOs/Catalog/ProductAttributeCombinationDto.cs
@@ -5,6 +5,7 @@ namespace Grand.Api.DTOs.Catalog
     public partial class ProductAttributeCombinationDto : BaseApiEntityModel
     {        
         public int StockQuantity { get; set; }
+        public int ReservedQuantity { get; set; }
         public bool AllowOutOfStockOrders { get; set; }
         public string Text { get; set; }
         public string Sku { get; set; }
