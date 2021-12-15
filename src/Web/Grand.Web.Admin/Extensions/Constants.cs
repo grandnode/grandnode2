04da2f99a68ed06cb735c7f1238e5f9358c9f805
KrzysztofPajak <krzysiek@grandnode.com>
2021-12-15 10:55:47 +0100

    Remove unused variable

--- src/Web/Grand.Web.Admin/Extensions/Constants.cs
+++ src/Web/Grand.Web.Admin/Extensions/Constants.cs
@@ -5,7 +5,6 @@
         public const string AreaAdmin = "Admin";
 
         public static string Layout_Admin => $"~/Areas/{AreaAdmin}/Views/Shared/_AdminLayout.cshtml";
-        public static string Layout_AdminPopup => $"~/Areas/{AreaAdmin}/Views/Shared/_AdminPopupLayout.cshtml";
         public static string Layout_AdminLogin => $"~/Areas/{AreaAdmin}/Views/Shared/_AdminLoginLayout.cshtml";
     }
 }
