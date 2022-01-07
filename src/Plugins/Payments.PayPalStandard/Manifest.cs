82d10edc1d4f3456564a5003aa11e88794cb04f7
KrzysztofPajak <krzysiek@grandnode.com>
2022-01-07 18:47:33 +0100

    Paypal plugin - update version

--- src/Plugins/Payments.PayPalStandard/Manifest.cs
+++ src/Plugins/Payments.PayPalStandard/Manifest.cs
@@ -8,5 +8,5 @@ using Payments.PayPalStandard;
     SystemName = PayPalStandardPaymentDefaults.ProviderSystemName,
     SupportedVersion = GrandVersion.SupportedPluginVersion,
     Author = "grandnode team",
-    Version = "1.00"
+    Version = "1.01"
 )]
