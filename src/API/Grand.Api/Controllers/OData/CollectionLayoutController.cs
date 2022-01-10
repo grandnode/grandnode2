61669c026b3ee298ec3d42c58638250932ac4d11
KrzysztofPajak <krzysiek@grandnode.com>
2022-01-10 12:39:09 +0100

    API - CollectionLayout - add ProducesResponseType

--- src/API/Grand.Api/Controllers/OData/CollectionLayoutController.cs
+++ src/API/Grand.Api/Controllers/OData/CollectionLayoutController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Query;
 using Swashbuckle.AspNetCore.Annotations;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Grand.Api.Controllers.OData
@@ -23,6 +24,9 @@ namespace Grand.Api.Controllers.OData
 
         [SwaggerOperation(summary: "Get entity from CollectionLayout by key", OperationId = "GetCollectionLayoutById")]
         [HttpGet("{key}")]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> Get(string key)
         {
             if (!await _permissionService.Authorize(PermissionSystemName.Maintenance))
@@ -38,6 +42,8 @@ namespace Grand.Api.Controllers.OData
         [SwaggerOperation(summary: "Get entities from CollectionLayout", OperationId = "GetCollectionLayouts")]
         [HttpGet]
         [EnableQuery(HandleNullPropagation = HandleNullPropagationOption.False)]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
         public async Task<IActionResult> Get()
         {
             if (!await _permissionService.Authorize(PermissionSystemName.Maintenance))
