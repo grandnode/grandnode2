91ac86a9d04973de7b7c3142a5413c95072a6657
support <support@grandnode.com>
2022-02-27 20:50:09 +0100

    Add tests

---
+++ src/Tests/Grand.Data.Tests/MongoDb/MongoDBRepositoryTest.cs
@@ -0,0 +1,17 @@
+﻿using Grand.Domain;
+using Grand.Domain.Data;
+using Grand.Domain.Data.Mongo;
+
+namespace Grand.Data.Tests.MongoDb
+{
+    public partial class MongoDBRepositoryTest<T> : MongoRepository<T>, IRepository<T> where T : BaseEntity
+    {
+        public MongoDBRepositoryTest():base(DriverTestConfiguration.Client.GetDatabase(DriverTestConfiguration.DatabaseNamespace.DatabaseName))
+        {
+            var client = DriverTestConfiguration.Client;
+            _database = client.GetDatabase(DriverTestConfiguration.DatabaseNamespace.DatabaseName);
+            _database.DropCollection(typeof(T).Name);
+            _collection = _database.GetCollection<T>(typeof(T).Name);
+        }
+    }
+}
---
+++ src/Tests/Grand.Data.Tests/MongoDb/MongoRepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Grand.Domain.Data;
+using Grand.SharedKernel.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grand.Data.Tests.MongoDb
+{
+    [TestClass()]
+    public class MongoRepositoryTests
+    {
+        private IRepository<SampleCollection> _myRepository;
+
+        [TestInitialize()]
+        public void Init()
+        {
+            _myRepository = new MongoDBRepositoryTest<SampleCollection>();
+
+            CommonPath.BaseDirectory = "";
+        }
+
+        [TestMethod()]
+        public void Insert_MongoRepository_Success()
+        {
+            var product = new SampleCollection();
+            _myRepository.Insert(product);
+
+            Assert.AreEqual(1, _myRepository.Table.Count());
+        }
+
+        [TestMethod()]
+        public async Task InsertAsync_MongoRepository_Success()
+        {
+            var product = new SampleCollection();
+            await _myRepository.InsertAsync(product);
+
+            Assert.AreEqual(1, _myRepository.Table.Count());
+        }
+    }
+}
