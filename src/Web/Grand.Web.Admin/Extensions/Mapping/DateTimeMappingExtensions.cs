e0ea3e5d66ee999b9fe4780b6dc3b68dceba1bf2
support <support@grandnode.com>
2022-03-05 21:08:15 +0100

    Update convert datetime

--- src/Web/Grand.Web.Admin/Extensions/Mapping/DateTimeMappingExtensions.cs
+++ src/Web/Grand.Web.Admin/Extensions/Mapping/DateTimeMappingExtensions.cs
@@ -7,8 +7,9 @@ namespace Grand.Web.Admin.Extensions
         public static DateTime? ConvertToUserTime(this DateTime? datetime, IDateTimeService dateTimeService)
         {
             if (datetime.HasValue)
-            {
-                datetime = dateTimeService.ConvertToUserTime(datetime.Value, TimeZoneInfo.Utc, dateTimeService.CurrentTimeZone);
+            {           
+                if(datetime.Value.Kind == DateTimeKind.Utc)
+                    datetime = dateTimeService.ConvertToUserTime(datetime.Value, TimeZoneInfo.Utc, dateTimeService.CurrentTimeZone);
             }
             return datetime;
         }
@@ -17,14 +18,18 @@ namespace Grand.Web.Admin.Extensions
         {
             if (datetime.HasValue)
             {
-                datetime = dateTimeService.ConvertToUtcTime(datetime.Value, dateTimeService.CurrentTimeZone);
+                if (datetime.Value.Kind == DateTimeKind.Local)
+                    datetime = dateTimeService.ConvertToUtcTime(datetime.Value, dateTimeService.CurrentTimeZone);
             }
             return datetime;
         }
 
         public static DateTime ConvertToUserTime(this DateTime datetime, IDateTimeService dateTimeService)
         {
-            return dateTimeService.ConvertToUserTime(datetime, TimeZoneInfo.Utc, dateTimeService.CurrentTimeZone);
+            if(datetime.Kind == DateTimeKind.Utc)
+                return dateTimeService.ConvertToUserTime(datetime, TimeZoneInfo.Utc, dateTimeService.CurrentTimeZone);
+
+            return datetime;
         }
 
         public static DateTime ConvertToUtcTime(this DateTime datetime, IDateTimeService dateTimeService)
