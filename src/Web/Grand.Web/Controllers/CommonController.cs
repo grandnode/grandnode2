ba36c0013a3e8faee43667018c87b3fa5d86f7d4
Krzysztof Pajak <krzysztof.pajak@epoczta.pl>
2022-06-04 10:20:50 +0200

    Clear cart returns html instead of json #258  (#272)

--- src/Web/Grand.Web/Controllers/CommonController.cs
+++ src/Web/Grand.Web/Controllers/CommonController.cs
@@ -127,6 +127,16 @@ namespace Grand.Web.Controllers
             return View();
         }
 
+        public virtual IActionResult Route(string routeName)
+        {
+            if (string.IsNullOrEmpty(routeName))
+                return Json(new { redirectToUrl = string.Empty });
+
+            var url = Url.RouteUrl(routeName);
+
+            return Json(new { redirectToUrl = url });
+        }
+
         //external authentication error
         public virtual IActionResult ExternalAuthenticationError(IEnumerable<string> errors)
         {
