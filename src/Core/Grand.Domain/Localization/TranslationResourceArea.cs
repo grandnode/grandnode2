a9e0b47bc69abca4865ae0d8f9fef0bfb315c5cd
support <support@grandnode.com>
2022-06-18 11:52:24 +0200

    Init

---
+++ src/Core/Grand.Domain/Localization/TranslationResourceArea.cs
@@ -0,0 +1,11 @@
+﻿namespace Grand.Domain.Localization
+{
+    public enum TranslationResourceArea
+    {
+        None = 0,
+        Admin = 1,
+        Front = 2,
+        Plugin = 3,
+        Other = 4
+    }
+}
