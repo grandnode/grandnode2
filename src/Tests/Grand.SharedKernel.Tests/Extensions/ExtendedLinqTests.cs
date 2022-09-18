2bd854f3e5367093d159692b6c1a117c3cf60625
support <support@grandnode.com>
2022-09-18 18:07:02 +0200

    Add tests

---
+++ src/Tests/Grand.SharedKernel.Tests/Extensions/ExtendedLinqTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grand.SharedKernel.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grand.SharedKernel.Tests;
+
+namespace Grand.SharedKernel.Extensions.Tests
+{
+    [TestClass()]
+    public class ExtendedLinqTests
+    {
+        [TestMethod()]
+        public void CartesianProductTest()
+        {
+            var sampleList = new List<SampleObject>() { 
+                new SampleObject() { Id = "1", Name = "name1" }, 
+                new SampleObject() { Id = "1", Name = "name2" },
+                new SampleObject() { Id = "3", Name = "name3" }
+            };
+
+            var cartesianProduct = sampleList.GroupBy(x=>x.Id).CartesianProduct().ToList();
+            Assert.AreEqual(2, cartesianProduct.Count);
+        }
+    }
+}
---
+++ src/Tests/Grand.SharedKernel.Tests/SampleObject.cs
@@ -0,0 +1,8 @@
+﻿namespace Grand.SharedKernel.Tests;
+
+public class SampleObject
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+
+}
