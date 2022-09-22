8c361d9e0c26075927b246d8115e7a0a965f9926
support <support@grandnode.com>
2022-09-22 19:35:18 +0200

    Add FluentValidationFilterTests

---
+++ src/Tests/Grand.Infrastructure.Tests/Validators/SourceTestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Grand.Infrastructure.Tests.Validators
+{
+    public class SourceTestValidator : AbstractValidator<SourceTest>
+    {
+        public SourceTestValidator()
+        {
+            RuleFor(person => person.LastName).NotNull();
+        }
+    }
+}
