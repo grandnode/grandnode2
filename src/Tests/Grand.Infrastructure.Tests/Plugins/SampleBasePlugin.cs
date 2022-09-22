b4fdbdf4dc4888c44f7fc184121f317dc7f7a4a3
support <support@grandnode.com>
2022-09-22 20:36:04 +0200

    Add plugin tests

---
+++ src/Tests/Grand.Infrastructure.Tests/Plugins/SampleBasePlugin.cs
@@ -0,0 +1,30 @@
+﻿using Grand.Infrastructure.Plugins;
+
+namespace Grand.Infrastructure.Tests.Plugins
+{
+    public class SampleBasePlugin : BasePlugin, IPlugin
+    {
+        private PluginInfo _pluginInfo;
+        public SampleBasePlugin()
+        {
+            _pluginInfo = new PluginInfo() {
+                Author = "grandnode",
+                DisplayOrder = 0,
+                FriendlyName = "sample plugin",
+                SystemName = "SamplePlugin"
+            };
+        }
+        public override PluginInfo PluginInfo {
+            get {
+                return _pluginInfo;
+            }
+            set {
+                _pluginInfo = value;
+            }
+        }
+        public override string ConfigurationUrl()
+        {
+            return "sample";
+        }
+    }
+}
