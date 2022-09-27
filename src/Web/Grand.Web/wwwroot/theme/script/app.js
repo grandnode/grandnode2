30fb90021654fb7f85ca2558b2d275d6bfa9ac1e
NamhoaiNgo <NamhoaiNgo@Melaleuca.com>
2022-09-27 17:49:02 +0800

    ismobile detect

--- src/Web/Grand.Web/wwwroot/theme/script/app.js
+++ src/Web/Grand.Web/wwwroot/theme/script/app.js
@@ -374,7 +374,29 @@ var vm = new Vue({
             }
         },
         isMobile: function () {
-            return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+            var hasTouchScreen = false;
+
+            if ("maxTouchPoints" in navigator) {
+                hasTouchScreen = navigator.maxTouchPoints > 0;
+            } else if ("msMaxTouchPoints" in navigator) {
+                hasTouchScreen = navigator.msMaxTouchPoints > 0;
+            } else {
+                var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+                if (mQ && mQ.media === "(pointer:coarse)") {
+                    hasTouchScreen = !!mQ.matches;
+                } else if ('orientation' in window) {
+                    hasTouchScreen = true; // deprecated, but good fallback
+                } else {
+                    // Only as a last resort, fall back to user agent sniffing
+                    var UA = navigator.userAgent;
+                    hasTouchScreen = (
+                        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+                        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+                    );
+                }
+            }
+
+            return hasTouchScreen;
         },
         attrchange: function (productId, loadPicture) {
             var form = document.getElementById('product-details-form');
