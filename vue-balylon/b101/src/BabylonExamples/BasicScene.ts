4d0f050d76bcb2ff3586c2156c011c956fb5795e
namhoaingo <namhoaingo@gmail.com>
2022-10-06 23:12:13 +0800

    add model

--- vue-balylon/b101/src/BabylonExamples/BasicScene.ts
+++ vue-balylon/b101/src/BabylonExamples/BasicScene.ts
@@ -1,39 +1,52 @@
-import { AxesViewer, Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Vector2, Vector3 } from "@babylonjs/core";
+import { ArcRotateCamera, AssetsManager, AxesViewer, Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Tools, Vector2, Vector3 } from "@babylonjs/core";
 
 export class BasicScene{
     scene: Scene;
     engine: Engine;
     
     constructor(private canvas: HTMLCanvasElement){
-        this.engine = new Engine(this.canvas, true);
-        this.scene = this.CreateScene();
-        this.engine.runRenderLoop(() =>{
-            this.scene.render();
-        })
+        // this.setEngine(canvas);
+
+        this.engine = new Engine(canvas, true);
+        this.scene = new Scene(this.engine);   
+
+
+        this.setCamera();
+        this.setLight();
+        // this.setMesh();    
+        // this.engine.runRenderLoop(() =>{
+            // this.scene.render();
+        // })
+        this.loadModel().load();
     }
 
-    CreateScene(): Scene{
-        const scene = new Scene(this.engine);
-        const camera = new FreeCamera("camera", new Vector3(3,1,0), this.scene);
-        camera.cameraRotation = new Vector2(0,0);
-        camera.rotation = new Vector3(0,4.7,0);
-        // move camera with the mouse
-        camera.attachControl();
-        camera.setTarget(new Vector3(0,1,0));
-        // light
-        const hemiLight = new HemisphericLight("hemiLight", new Vector3(0,1,0), this.scene );
-        hemiLight.intensity = 0.5;
+    setCamera(){
+        const camera = new ArcRotateCamera("Camera", Math.PI * 2, Tools.ToRadians(80), 15, new Vector3(0,3,-3), this.scene);
+        camera.setTarget(new Vector3(0, 1,0));
+        camera.attachControl();        
+    }
 
-        // 3 D object
+    setLight(){
+        const hemiLight = new HemisphericLight("hemiLight", new Vector3(0,1,0), this.scene);
+    }
+
+    setMesh(){
         const ground = MeshBuilder.CreateGround("ground", {width: 10, height: 10}, this.scene);
-        
-        const ball = MeshBuilder.CreateSphere("ball",{diameter:1}, this.scene);
-        ball.position = new Vector3(0,1,0); 
-        // Add a mesh
-        // const axes = new AxesViewer(this.scene, 4);
-
-        
-        return scene;
-
+        const ball = MeshBuilder.CreateSphere("ball", {diameter:1}, this.scene);
+        ball.position = new Vector3(0,1,0);
     }
+
+    loadModel(){
+        const loader = new AssetsManager(this.scene);
+        const loadHelmetModel = loader.addMeshTask("helmet", "", "", "12318_Helmet_v1_L3.obj");
+        loadHelmetModel.onSuccess= (t)=>{
+            this.engine.runRenderLoop(() =>{
+                this.scene.render();
+            })
+        }
+
+        return loader;
+    }
+
+
 }
