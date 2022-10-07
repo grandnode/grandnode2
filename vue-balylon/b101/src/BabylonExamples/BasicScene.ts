182e325cc3061c1fc918bf992d37173a0c566b9e
Nam Hoai Ngo <NamhoaiNgo@Melaleuca.com>
2022-10-07 18:19:47 +0800

    add babylon

--- vue-balylon/b101/src/BabylonExamples/BasicScene.ts
+++ vue-balylon/b101/src/BabylonExamples/BasicScene.ts
@@ -1,4 +1,6 @@
-import { ArcRotateCamera, AssetsManager, AxesViewer, Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Tools, Vector2, Vector3 } from "@babylonjs/core";
+import { ArcRotateCamera, AssetsManager, AxesViewer, Color3, Engine, FreeCamera, HemisphericLight, Material, MeshBuilder, Scene, StandardMaterial, Tools, Vector2, Vector3 } from "@babylonjs/core";
+import { ThinSprite } from "@babylonjs/core/Sprites/thinSprite";
+import 'babylonjs-loaders';
 
 export class BasicScene{
     scene: Scene;
@@ -17,11 +19,13 @@ export class BasicScene{
         // this.engine.runRenderLoop(() =>{
             // this.scene.render();
         // })
+        this.setMeshColor();
         this.loadModel().load();
+        // this.setMeshColor()
     }
 
     setCamera(){
-        const camera = new ArcRotateCamera("Camera", Math.PI * 2, Tools.ToRadians(80), 15, new Vector3(0,3,-3), this.scene);
+        const camera = new ArcRotateCamera("Camera", Math.PI * 2, Tools.ToRadians(80), 40, new Vector3(10,3,-3), this.scene);
         camera.setTarget(new Vector3(0, 1,0));
         camera.attachControl();        
     }
@@ -38,15 +42,27 @@ export class BasicScene{
 
     loadModel(){
         const loader = new AssetsManager(this.scene);
-        const loadHelmetModel = loader.addMeshTask("helmet", "", "", "12318_Helmet_v1_L3.obj");
+        const loadHelmetModel = loader.addMeshTask("helmetTask", "helmet", "", "helmet.babylon");
         loadHelmetModel.onSuccess= (t)=>{
+            
             this.engine.runRenderLoop(() =>{
+
                 this.scene.render();
             })
         }
 
         return loader;
     }
+    
+    setMeshColor(){
+            const helmet = this.scene.getMeshByName("helmet")!;
 
+            const material = new StandardMaterial("material", this.scene);
+            material.diffuseColor = new Color3(1,0,1);
+            material.specularColor = new Color3(0.5, 0.6, 0.87);
+            material.emissiveColor = new Color3(1,1,1);
+            material.ambientColor = new Color3(0.23, 0.98, 0.53);
+            helmet.material = material;
+    }
 
 }
