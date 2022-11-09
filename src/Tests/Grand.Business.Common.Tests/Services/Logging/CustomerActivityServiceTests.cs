756730b4b462e20a32ee12aff1f46595cc441d9d
support <support@grandnode.com>
2022-11-09 18:32:28 +0100

    Fix test

--- src/Tests/Grand.Business.Common.Tests/Services/Logging/CustomerActivityServiceTests.cs
+++ src/Tests/Grand.Business.Common.Tests/Services/Logging/CustomerActivityServiceTests.cs
@@ -242,8 +242,7 @@ namespace Grand.Business.Common.Services.Logging.Tests
         public async Task ClearAllActivitiesTest()
         {
             //Arrange
-            await InsertActivityType();
-            await _customerActivityService.InsertActivity("test", "1", new Customer(), "", "");
+            await _repository.InsertAsync(new ActivityLog() { ActivityLogTypeId = "1", EntityKeyId = "1" });
             //Act
             await _customerActivityService.ClearAllActivities();
             //Assert
