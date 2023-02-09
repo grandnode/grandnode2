fe60c23014b1c98e59c62af3fdb4812be30b8eb9
support <support@grandnode.com>
2023-02-09 21:06:19 +0100

    Allow to use swagger for frontent API / generate openapi

--- src/API/Grand.Api/Extensions/AddParamOperationFilter.cs
+++ src/API/Grand.Api/Extensions/AddParamOperationFilter.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using MassTransit.Internals;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Grand.Api.Extensions
@@ -7,66 +9,90 @@ namespace Grand.Api.Extensions
     {
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
-            if (context.ApiDescription.GroupName == "v1" && context.ApiDescription.HttpMethod == "GET" && context.ApiDescription.ParameterDescriptions.Count == 0)
+            if (context.ApiDescription.GroupName == "v1" && context.ApiDescription.HttpMethod == "GET" &&
+                context.ApiDescription.ParameterDescriptions.Count == 0)
             {
                 ApplyV1(operation, context);
+            }
+
+            if (context.ApiDescription.GroupName == "v2" && context.ApiDescription.HttpMethod == "POST")
+            {
+                ApplyV2(operation, context);
             }
         }
 
         private void ApplyV1(OpenApiOperation operation, OperationFilterContext context)
         {
             operation.Parameters.Add(new OpenApiParameter() {
-                    Name = "$top",
-                    AllowReserved = true,
-                    In = ParameterLocation.Query,
-                    Description = "Show only the first n items.",
-                    Required = false,
-                    Schema = new OpenApiSchema() {
-                        Minimum = 0,
-                        Type = "integer"
+                Name = "$top",
+                AllowReserved = true,
+                In = ParameterLocation.Query,
+                Description = "Show only the first n items.",
+                Required = false,
+                Schema = new OpenApiSchema() {
+                    Minimum = 0,
+                    Type = "integer"
+                }
+            });
+            operation.Parameters.Add(new OpenApiParameter() {
+                Name = "$skip",
+                AllowReserved = true,
+                In = ParameterLocation.Query,
+                Description = "Skip the first n items",
+                Required = false,
+                Schema = new OpenApiSchema() {
+                    Minimum = 0,
+                    Type = "integer"
+                }
+            });
+            operation.Parameters.Add(new OpenApiParameter() {
+                Name = "$count",
+                AllowReserved = true,
+                In = ParameterLocation.Query,
+                Description = "Include count of items",
+                Required = false,
+                Schema = new OpenApiSchema() {
+                    Type = "boolean"
+                }
+            });
+            operation.Parameters.Add(new OpenApiParameter() {
+                Name = "$orderby",
+                AllowReserved = true,
+                In = ParameterLocation.Query,
+                Description = "Order items by property values",
+                Required = false,
+                Schema = new OpenApiSchema() {
+                    Type = "string"
+                }
+            });
+            operation.Parameters.Add(new OpenApiParameter() {
+                Name = "$filter",
+                AllowReserved = true,
+                In = ParameterLocation.Query,
+                Description = "Filter items by property values",
+                Required = false,
+                Schema = new OpenApiSchema() {
+                    Type = "string"
+                }
+            });
+        }
+
+        private void ApplyV2(OpenApiOperation operation, OperationFilterContext context)
+        {
+            operation.Parameters ??= new List<OpenApiParameter>();
+
+            if (context.MethodInfo.GetAttribute<AutoValidateAntiforgeryTokenAttribute>().Any())
+            {
+                operation.Parameters.Add(new OpenApiParameter {
+                    Name = "X-CSRF-TOKEN",
+                    In = ParameterLocation.Header,
+                    Required = true,
+                    Schema = new OpenApiSchema {
+                        Type = "string",
+                        Description = "Antiforgery token"
                     }
                 });
-                operation.Parameters.Add(new OpenApiParameter() {
-                    Name = "$skip",
-                    AllowReserved = true,
-                    In = ParameterLocation.Query,
-                    Description = "Skip the first n items",
-                    Required = false,
-                    Schema = new OpenApiSchema() {
-                        Minimum = 0,
-                        Type = "integer"
-                    }
-                });
-                operation.Parameters.Add(new OpenApiParameter() {
-                    Name = "$count",
-                    AllowReserved = true,
-                    In = ParameterLocation.Query,
-                    Description = "Include count of items",
-                    Required = false,
-                    Schema = new OpenApiSchema() {
-                        Type = "boolean"
-                    }
-                });
-                operation.Parameters.Add(new OpenApiParameter() {
-                    Name = "$orderby",
-                    AllowReserved = true,
-                    In = ParameterLocation.Query,
-                    Description = "Order items by property values",
-                    Required = false,
-                    Schema = new OpenApiSchema() {
-                        Type = "string"
-                    }
-                });
-                operation.Parameters.Add(new OpenApiParameter() {
-                    Name = "$filter",
-                    AllowReserved = true,
-                    In = ParameterLocation.Query,
-                    Description = "Filter items by property values",
-                    Required = false,
-                    Schema = new OpenApiSchema() {
-                        Type = "string"
-                    }
-                });
+            }
         }
     }
 }
