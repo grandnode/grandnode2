0110b3117f7f595ec420f9e77b9b6ea978a9c14f
support <support@grandnode.com>
2023-02-15 20:44:46 +0100

    Fix - Error loading tier prices table when the customer group is deleted which was mapped to a tier price entry #363

--- src/Business/Grand.Business.Common/Events/GroupDeletedEventHandler.cs
+++ src/Business/Grand.Business.Common/Events/GroupDeletedEventHandler.cs
@@ -1,5 +1,8 @@
-﻿using Grand.Domain.Customers;
+﻿using Grand.Domain.Catalog;
+using Grand.Domain.Customers;
 using Grand.Domain.Data;
+using Grand.Infrastructure.Caching;
+using Grand.Infrastructure.Caching.Constants;
 using Grand.Infrastructure.Events;
 using MediatR;
 
@@ -8,16 +11,29 @@ namespace Grand.Business.Common.Events
     public class GroupDeletedEventHandler : INotificationHandler<EntityDeleted<CustomerGroup>>
     {
         private readonly IRepository<Customer> _customerRepository;
-
-        public GroupDeletedEventHandler(IRepository<Customer> customerRepository)
+        private readonly IRepository<Product> _productRepository;
+        private readonly ICacheBase _cacheBase;
+        
+        public GroupDeletedEventHandler(
+            IRepository<Customer> customerRepository,
+            IRepository<Product> productRepository,
+            ICacheBase cacheBase)
         {
             _customerRepository = customerRepository;
+            _productRepository = productRepository;
+            _cacheBase = cacheBase;
         }
 
         public async Task Handle(EntityDeleted<CustomerGroup> notification, CancellationToken cancellationToken)
         {
             //delete from customers
             await _customerRepository.Pull(string.Empty, x => x.Groups, notification.Entity.Id);
+            
+            //delete tier prices on the product
+            await _productRepository.PullFilter(string.Empty, x => x.TierPrices, z => z.CustomerGroupId, notification.Entity.Id);
+
+            //clear cache
+            await _cacheBase.RemoveByPrefix(CacheKey.PRODUCTS_PATTERN_KEY);
         }
     }
 }
