bf05fdadcffe4e94afec955159cac61f80e16dcd
support <support@grandnode.com>
2023-03-11 11:12:36 +0100

    Commit

--- src/Core/Grand.Infrastructure/GrandVersion.cs
+++ src/Core/Grand.Infrastructure/GrandVersion.cs
@@ -1,8 +1,9 @@
-﻿namespace Grand.Infrastructure
+﻿using System.Reflection;
+
+namespace Grand.Infrastructure
 {
     public static class GrandVersion
     {
-
         /// <summary>
         /// Gets the major version
         /// </summary>
@@ -36,11 +37,42 @@
         /// <summary>
         /// Gets the git branch
         /// </summary>
-        public const string GitBranch = ThisAssembly.Git.Branch;
-        
+        public static string GitBranch = GetGitBranch();
+
         /// <summary>
         /// Gets the git commit
         /// </summary>
-        public const string GitCommit = ThisAssembly.Git.Commit;
+        public static string GitCommit = GetGitHash();
+
+        private static string GetGitHash()
+        {
+            if (!string.IsNullOrEmpty(_gitHash)) return _gitHash;
+
+            var version = "1.0.0+LOCALBUILD"; 
+            var appAssembly = typeof(GrandVersion).Assembly;
+            var infoVerAttr = (AssemblyInformationalVersionAttribute)appAssembly
+                .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute)).FirstOrDefault();
+            if (infoVerAttr is { InformationalVersion.Length: > 6 })
+            {
+                version = infoVerAttr.InformationalVersion;
+            }
+            _gitHash = version[(version.IndexOf('+') + 1)..];
+            return _gitHash;
+        }
+
+        private static string _gitHash { get; set; }
+
+        private static string GetGitBranch()
+        {
+            if (!string.IsNullOrEmpty(_gitBranch)) return _gitBranch;
+
+            var appAssembly = typeof(GrandVersion).Assembly;
+            var infoBranchAttr = appAssembly.GetCustomAttributes(typeof(AssemblyMetadataAttribute))
+                .FirstOrDefault(x => ((AssemblyMetadataAttribute)x).Key == "BranchName");
+            _gitBranch = infoBranchAttr != null ? ((AssemblyMetadataAttribute)infoBranchAttr).Value : "UNKNOWN";
+            return _gitBranch;
+        }
+
+        private static string _gitBranch { get; set; }
     }
 }
