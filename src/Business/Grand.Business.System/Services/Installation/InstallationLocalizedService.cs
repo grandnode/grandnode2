713837b680841de1a4d4731dbd02bc1ee19e953e
support <support@grandnode.com>
2023-07-24 19:21:35 +0200

    Minor changes InstallationLocalizedService.cs

--- src/Business/Grand.Business.System/Services/Installation/InstallationLocalizedService.cs
+++ src/Business/Grand.Business.System/Services/Installation/InstallationLocalizedService.cs
@@ -91,11 +91,11 @@ namespace Grand.Business.System.Services.Installation
 
                 var languageNode = xmlDocument.SelectSingleNode(@"//Language");
 
-                if (languageNode == null || languageNode.Attributes == null)
+                if (languageNode?.Attributes == null)
                     continue;
 
                 //get language friendly name
-                var languageName = languageNode.Attributes["Name"].InnerText.Trim();
+                var languageName = languageNode.Attributes["Name"]?.InnerText.Trim();
 
                 //is default
                 var isDefaultAttribute = languageNode.Attributes["IsDefault"];
@@ -159,18 +159,18 @@ namespace Grand.Business.System.Services.Installation
 
             _availableCollation = new List<InstallationCollation>();
             var filePath = CommonPath.MapPath("App_Data/Resources/supportedcollation.xml");
-            var xmlDocument = new XmlDocument();
+            var xmlDocument = new XmlDocument { XmlResolver = null };
             xmlDocument.Load(File.OpenRead(filePath));
 
             var collation = xmlDocument.SelectNodes(@"//Collations/Collation");
 
-            foreach (XmlNode resNode in collation)
+            foreach (XmlNode resNode in collation!)
             {
-                var resNameAttribute = resNode.Attributes["Name"];
+                var resNameAttribute = resNode.Attributes!["Name"];
                 var resValueNode = resNode.SelectSingleNode("Value");
 
-                var resourceName = resNameAttribute.Value.Trim();
-                var resourceValue = resValueNode.InnerText.Trim();
+                var resourceName = resNameAttribute!.Value.Trim();
+                var resourceValue = resValueNode!.InnerText.Trim();
 
                 _availableCollation.Add(new InstallationCollation() {
                     Name = resourceName,
