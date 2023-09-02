04b12092de72d93f6ed371520d05792557490e50
support <support@grandnode.com>
2023-09-02 21:03:45 +0200

    Remove unused constant

--- src/API/Grand.Api/Constants/Configurations.cs
+++ src/API/Grand.Api/Constants/Configurations.cs
@@ -2,7 +2,6 @@
 {
     public static class Configurations
     {
-        public const string ODataRouteName = "ODataRoute";
         public const string ODataRoutePrefix = "odata";
         public const string ODataModelBuilderNamespace = "Default";
         public const string CorsPolicyName = "CorsPolicy";
