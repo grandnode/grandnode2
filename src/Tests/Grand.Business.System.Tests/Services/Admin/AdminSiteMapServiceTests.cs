30b78c24eaa3598e8025691291692d5e3a27cb52
support <support@grandnode.com>
2023-09-30 20:45:44 +0200

    Fix AdminSiteMapServiceTests

--- src/Tests/Grand.Business.System.Tests/Services/Admin/AdminSiteMapServiceTests.cs
+++ src/Tests/Grand.Business.System.Tests/Services/Admin/AdminSiteMapServiceTests.cs
@@ -2,6 +2,7 @@
 using Grand.Domain.Admin;
 using Grand.Domain.Data;
 using Grand.Infrastructure.Caching;
+using MediatR;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -13,13 +14,15 @@ namespace Grand.Business.System.Tests.Services.Admin
         private Mock<IRepository<AdminSiteMap>> _repositoryMock;
         private Mock<ICacheBase> _cacheMock;
         private AdminSiteMapService _service;
-
+        private Mock<IMediator> _mediatorMock;
+        
         [TestInitialize]
         public void Init()
         {
             _repositoryMock = new Mock<IRepository<AdminSiteMap>>();
             _cacheMock = new Mock<ICacheBase>();
-            _service = new AdminSiteMapService(_repositoryMock.Object,_cacheMock.Object);
+            _mediatorMock = new Mock<IMediator>();
+            _service = new AdminSiteMapService(_repositoryMock.Object,_cacheMock.Object, _mediatorMock.Object);
         }
 
         [TestMethod]
