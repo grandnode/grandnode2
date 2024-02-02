2c3d4d21755cab61fa5d0c4d15f7f8447b0685cf
support <support@grandnode.com>
2024-02-02 20:34:14 +0100

    Add stripe plugin

---
+++ src/Plugins/Payments.StripeCheckout/Models/ConfigurationModel.cs
@@ -0,0 +1,24 @@
+﻿using Grand.Infrastructure.ModelBinding;
+using Grand.Infrastructure.Models;
+
+namespace Payments.StripeCheckout.Models;
+
+public class ConfigurationModel : BaseModel
+{
+    public string StoreScope { get; set; }
+    
+    [GrandResourceDisplayName("Plugins.Payments.StripeCheckout.Fields.ApiKey")]
+    public string ApiKey { get; set; }
+    
+    [GrandResourceDisplayName("Plugins.Payments.StripeCheckout.Fields.WebhookEndpointSecret")]
+    public string WebhookEndpointSecret { get; set; }
+
+    [GrandResourceDisplayName("Plugins.Payments.StripeCheckout.Fields.Description")]
+    public string Description { get; set; }
+
+    [GrandResourceDisplayName("Plugins.Payments.StripeCheckout.Fields.Line")]
+    public string Line { get; set; }
+
+    [GrandResourceDisplayName("Plugins.Payments.StripeCheckout.Fields.DisplayOrder")]
+    public int DisplayOrder { get; set; }
+}
