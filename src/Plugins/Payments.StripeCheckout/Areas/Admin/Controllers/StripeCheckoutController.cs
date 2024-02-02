20cbdd82081bd26de4030a08f3710a2d66c41650
support <support@grandnode.com>
2024-02-02 20:46:06 +0100

    Update

--- src/Plugins/Payments.StripeCheckout/Areas/Admin/Controllers/StripeCheckoutController.cs
+++ src/Plugins/Payments.StripeCheckout/Areas/Admin/Controllers/StripeCheckoutController.cs
@@ -31,74 +31,75 @@ public class StripeCheckoutController : BasePaymentController
         ITranslationService translationService,
         IPermissionService permissionService)
     {
-            _workContext = workContext;
-            _storeService = storeService;
-            _settingService = settingService;
-            _translationService = translationService;
-            _permissionService = permissionService;
-        }
+        _workContext = workContext;
+        _storeService = storeService;
+        _settingService = settingService;
+        _translationService = translationService;
+        _permissionService = permissionService;
+    }
 
     private async Task<string> GetActiveStore()
     {
-            var stores = await _storeService.GetAllStores();
-            if (stores.Count < 2)
-                return stores.FirstOrDefault()?.Id;
+        var stores = await _storeService.GetAllStores();
+        if (stores.Count < 2)
+            return stores.FirstOrDefault()?.Id;
 
-            var storeId = _workContext.CurrentCustomer.GetUserFieldFromEntity<string>(SystemCustomerFieldNames.AdminAreaStoreScopeConfiguration);
-            var store = await _storeService.GetStoreById(storeId);
+        var storeId =
+            _workContext.CurrentCustomer.GetUserFieldFromEntity<string>(SystemCustomerFieldNames
+                .AdminAreaStoreScopeConfiguration);
+        var store = await _storeService.GetStoreById(storeId);
 
-            return store != null ? store.Id : "";
-        }
+        return store != null ? store.Id : "";
+    }
 
     public async Task<IActionResult> Configure()
     {
-            if (!await _permissionService.Authorize(StandardPermission.ManagePaymentMethods))
-                return AccessDeniedView();
+        if (!await _permissionService.Authorize(StandardPermission.ManagePaymentMethods))
+            return AccessDeniedView();
 
-            //load settings for a chosen store scope
-            var storeScope = await GetActiveStore();
-            var stripeCheckoutPaymentSettings = _settingService.LoadSetting<StripeCheckoutPaymentSettings>(storeScope);
+        //load settings for a chosen store scope
+        var storeScope = await GetActiveStore();
+        var stripeCheckoutPaymentSettings = _settingService.LoadSetting<StripeCheckoutPaymentSettings>(storeScope);
 
-            var model = new ConfigurationModel {
-                ApiKey = stripeCheckoutPaymentSettings.ApiKey,
-                WebhookEndpointSecret = stripeCheckoutPaymentSettings.WebhookEndpointSecret,
-                Description = stripeCheckoutPaymentSettings.Description,
-                Line = stripeCheckoutPaymentSettings.Line,
-                DisplayOrder = stripeCheckoutPaymentSettings.DisplayOrder,
-                StoreScope = storeScope
-            };
+        var model = new ConfigurationModel {
+            ApiKey = stripeCheckoutPaymentSettings.ApiKey,
+            WebhookEndpointSecret = stripeCheckoutPaymentSettings.WebhookEndpointSecret,
+            Description = stripeCheckoutPaymentSettings.Description,
+            Line = stripeCheckoutPaymentSettings.Line,
+            DisplayOrder = stripeCheckoutPaymentSettings.DisplayOrder,
+            StoreScope = storeScope
+        };
 
-            return View(model);
-        }
+        return View(model);
+    }
 
     [HttpPost]
     public async Task<IActionResult> Configure(ConfigurationModel model)
     {
-            if (!await _permissionService.Authorize(StandardPermission.ManagePaymentMethods))
-                return AccessDeniedView();
+        if (!await _permissionService.Authorize(StandardPermission.ManagePaymentMethods))
+            return AccessDeniedView();
 
-            if (!ModelState.IsValid)
-                return await Configure();
-
-            //load settings for a chosen store scope
-            var storeScope = await GetActiveStore();
-            var stripeCheckoutPaymentSettings = _settingService.LoadSetting<StripeCheckoutPaymentSettings>(storeScope);
-
-            //save settings
-            stripeCheckoutPaymentSettings.ApiKey = model.ApiKey;
-            stripeCheckoutPaymentSettings.WebhookEndpointSecret = model.WebhookEndpointSecret;
-            stripeCheckoutPaymentSettings.Description = model.Description;
-            stripeCheckoutPaymentSettings.Line = model.Line;
-            stripeCheckoutPaymentSettings.DisplayOrder = model.DisplayOrder;
-
-            await _settingService.SaveSetting(stripeCheckoutPaymentSettings, storeScope);
-
-            //now clear settings cache
-            await _settingService.ClearCache();
-
-            Success(_translationService.GetResource("Admin.Plugins.Saved"));
-
+        if (!ModelState.IsValid)
             return await Configure();
-        }
 
+        //load settings for a chosen store scope
+        var storeScope = await GetActiveStore();
+        var stripeCheckoutPaymentSettings = _settingService.LoadSetting<StripeCheckoutPaymentSettings>(storeScope);
+
+        //save settings
+        stripeCheckoutPaymentSettings.ApiKey = model.ApiKey;
+        stripeCheckoutPaymentSettings.WebhookEndpointSecret = model.WebhookEndpointSecret;
+        stripeCheckoutPaymentSettings.Description = model.Description;
+        stripeCheckoutPaymentSettings.Line = model.Line;
+        stripeCheckoutPaymentSettings.DisplayOrder = model.DisplayOrder;
+
+        await _settingService.SaveSetting(stripeCheckoutPaymentSettings, storeScope);
+
+        //now clear settings cache
+        await _settingService.ClearCache();
+
+        Success(_translationService.GetResource("Admin.Plugins.Saved"));
+
+        return await Configure();
+    }
 }
