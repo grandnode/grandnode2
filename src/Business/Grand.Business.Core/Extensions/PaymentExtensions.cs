08669f71e8a65e908fe94258f55a31a92ae1609c
KrzysztofPajak <krzysiek@grandnode.com>
2024-08-25 14:20:31 +0200

    Remove unused CalculateAdditionalFee method in PaymentExtensions.cs

--- src/Business/Grand.Business.Core/Extensions/PaymentExtensions.cs
+++ src/Business/Grand.Business.Core/Extensions/PaymentExtensions.cs
@@ -26,37 +26,4 @@ public static class PaymentExtensions
                paymentSettings.ActivePaymentProviderSystemNames.Any(activeMethodSystemName =>
                    paymentMethod.SystemName.Equals(activeMethodSystemName, StringComparison.OrdinalIgnoreCase));
     }
-
-    /// <summary>
-    ///     Calculate payment method fee
-    /// </summary>
-    /// <param name="paymentMethod">Payment method</param>
-    /// <param name="orderTotalCalculationService">Order total calculation service</param>
-    /// <param name="cart">Shopping cart</param>
-    /// <param name="fee">Fee value</param>
-    /// <param name="usePercentage">Is fee amount specified as percentage or fixed value?</param>
-    /// <returns>Result</returns>
-    public static async Task<double> CalculateAdditionalFee(this IPaymentProvider paymentMethod,
-        IOrderCalculationService orderTotalCalculationService, IList<ShoppingCartItem> cart,
-        double fee, bool usePercentage)
-    {
-        ArgumentNullException.ThrowIfNull(paymentMethod);
-        if (fee <= 0)
-            return fee;
-
-        double result;
-        if (usePercentage)
-        {
-            //percentage
-            var shoppingCartSubTotal = await orderTotalCalculationService.GetShoppingCartSubTotal(cart, true);
-            result = (float)shoppingCartSubTotal.subTotalWithDiscount * (float)fee / 100f;
-        }
-        else
-        {
-            //fixed value
-            result = fee;
-        }
-
-        return result;
-    }
 }
