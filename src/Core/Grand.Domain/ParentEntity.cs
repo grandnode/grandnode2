5d7483f2ade3c8f2d9759d0fe05f6a03a2709850
KrzysztofPajak <krzysiek@grandnode.com>
2024-11-18 17:51:28 +0100

    Refactor ParentEntity to use private _id field for Id

--- src/Core/Grand.Domain/ParentEntity.cs
+++ src/Core/Grand.Domain/ParentEntity.cs
@@ -4,6 +4,16 @@ namespace Grand.Domain;
 
 public abstract class ParentEntity
 {
+    private string _id;
+
+    protected ParentEntity()
+    {
+        _id = UniqueIdentifier.New;
+    }
+
     [DBFieldName("_id")]
-    public string Id { get; set; } = UniqueIdentifier.New;
+    public string Id {
+        get => _id;
+        set => _id = string.IsNullOrEmpty(value) ? UniqueIdentifier.New : value;
+    }   
 }
