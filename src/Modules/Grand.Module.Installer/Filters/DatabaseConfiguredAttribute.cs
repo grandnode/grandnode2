1557169628b94a60b4b668c4cf271f093862671b
KrzysztofPajak <krzysiek@grandnode.com>
2024-11-26 19:42:25 +0100

    Refactor InstallController and add DatabaseConfigured attribute Added DatabaseConfiguredAttribute.cs: - Defined DatabaseConfiguredAttribute implementing IAsyncActionFilter to check database installation and handle action accordingly.

---
+++ src/Modules/Grand.Module.Installer/Filters/DatabaseConfiguredAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Grand.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Grand.Module.Installer.Filters;
+
+public class DatabaseConfiguredAttribute : Attribute, IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!DataSettingsManager.DatabaseIsInstalled())
+        {
+            await next();
+        }
+        else
+        {
+            context.Result = new NotFoundResult();
+        }
+    }
+}
