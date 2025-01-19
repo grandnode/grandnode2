ad0e5e23d0fe21ec1db0a770b549bc814532f1e7
KrzysztofPajak <krzysiek@grandnode.com>
2025-01-19 15:48:13 +0100

    Update aspire

--- src/Aspire/Aspire.AppHost/Program.cs
+++ src/Aspire/Aspire.AppHost/Program.cs
@@ -1,16 +1,9 @@
+using Aspire.AppHost;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-
-var mongo = builder.AddMongoDB("mongo")
-                   .WithLifetime(ContainerLifetime.Persistent);
-
+var mongo = builder.AddMongoDB("mongo").WithLifetime(ContainerLifetime.Persistent);
 var mongodb = mongo.AddDatabase("Mongodb");
-
-builder
-    .AddProject<Projects.Grand_Web>("grand-web")
-    .WithHttpEndpoint(80)
-    .WithReference(mongodb)
-    .WaitFor(mongodb);
+builder.ConfigureGrandWebProject(mongodb);
 
 builder.Build().Run();
