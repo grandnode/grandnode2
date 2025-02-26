b3cacb3beee6c69e3b0360c3d9a2b8bb1f5a1301
KrzysztofPajak <krzysiek@grandnode.com>
2025-02-26 20:31:55 +0100

    Remove duplicated

--- src/Web/Grand.Web.Vendor/Controllers/SearchController.cs
+++ src/Web/Grand.Web.Vendor/Controllers/SearchController.cs
@@ -2,9 +2,8 @@
 using Grand.Business.Core.Interfaces.Catalog.Categories;
 using Grand.Business.Core.Interfaces.Catalog.Collections;
 using Grand.Domain.Admin;
-using Grand.Domain.Catalog;
 using Grand.Web.Common.DataSource;
-using Grand.Web.Vendor.Models.Common;
+using Grand.Web.Common.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Grand.Web.Vendor.Controllers;
@@ -29,97 +28,46 @@ public class SearchController : BaseVendorController
     [HttpGet]
     public async Task<IActionResult> Category(string categoryId, DataSourceRequestFilter model)
     {
-        async Task<IList<SearchModel>> PrepareModel(IEnumerable<Category> categories)
-        {
-            var model = new List<SearchModel>();
-            if (!string.IsNullOrEmpty(categoryId))
-            {
-                var currentCategory = await _categoryService.GetCategoryById(categoryId);
-                if (currentCategory != null)
-                    model.Add(new SearchModel {
-                        Id = currentCategory.Id,
-                        Name = await _categoryService.GetFormattedBreadCrumb(currentCategory)
-                    });
-            }
-
-            foreach (var item in categories)
-                if (item.Id != categoryId)
-                    model.Add(new SearchModel {
-                        Id = item.Id,
-                        Name = await _categoryService.GetFormattedBreadCrumb(item)
-                    });
-            return model;
-        }
-
         var categories = await _categoryService.GetAllCategories(
+            parentId: null,
             categoryName: model.GetNameFilterValue(),
-            pageSize: _adminSearchSettings.CategorySizeLimit);
-        var gridModel = new DataSourceResult {
-            Data = await PrepareModel(categories)
-        };
+            storeId: "",
+            pageIndex: 0,
+            pageSize: _adminSearchSettings.CategorySizeLimit,
+            showHidden: false
+        );
+
+        var gridModel = await DataSourceResultHelper.GetSearchResult(categoryId, categories, async category => await _categoryService.GetFormattedBreadCrumb(category));
         return Json(gridModel);
     }
 
     [HttpGet]
     public async Task<IActionResult> Collection(string collectionId, DataSourceRequestFilter model)
     {
-        async Task<IList<SearchModel>> PrepareModel(IEnumerable<Collection> collections)
-        {
-            var model = new List<SearchModel>();
-            if (!string.IsNullOrEmpty(collectionId))
-            {
-                var currentCollection = await _collectionService.GetCollectionById(collectionId);
-                if (currentCollection != null)
-                    model.Add(new SearchModel {
-                        Id = currentCollection.Id,
-                        Name = currentCollection.Name
-                    });
-            }
-
-            model.AddRange(from item in collections
-                where item.Id != collectionId
-                select new SearchModel { Id = item.Id, Name = item.Name });
-            return model;
-        }
-
         var collections = await _collectionService.GetAllCollections(
-            model.GetNameFilterValue(),
-            pageSize: _adminSearchSettings.CollectionSizeLimit);
+            collectionName: model.GetNameFilterValue(),
+            storeId: "",
+            pageIndex: 0,
+            pageSize: _adminSearchSettings.CollectionSizeLimit,
+            showHidden: false
+        );
 
-        var gridModel = new DataSourceResult {
-            Data = await PrepareModel(collections)
-        };
+        var gridModel = await DataSourceResultHelper.GetSearchResult(collectionId, collections, collection => Task.FromResult(collection.Name));
         return Json(gridModel);
     }
 
     [HttpGet]
     public async Task<IActionResult> Brand(string brandId, DataSourceRequestFilter model)
     {
-        async Task<IList<SearchModel>> PrepareModel(IEnumerable<Brand> brands)
-        {
-            var model = new List<SearchModel>();
-            if (!string.IsNullOrEmpty(brandId))
-            {
-                var currentBrand = await _brandService.GetBrandById(brandId);
-                if (currentBrand != null)
-                    model.Add(new SearchModel {
-                        Id = currentBrand.Id,
-                        Name = currentBrand.Name
-                    });
-            }
-
-            model.AddRange(from item in brands
-                where item.Id != brandId
-                select new SearchModel { Id = item.Id, Name = item.Name });
-            return model;
-        }
-
         var brands = await _brandService.GetAllBrands(
-            model.GetNameFilterValue(),
-            pageSize: _adminSearchSettings.BrandSizeLimit);
-        var gridModel = new DataSourceResult {
-            Data = await PrepareModel(brands)
-        };
+            brandName: model.GetNameFilterValue(),
+            storeId: "",
+            pageIndex: 0,
+            pageSize: _adminSearchSettings.BrandSizeLimit,
+            showHidden: false
+        );
+
+        var gridModel = await DataSourceResultHelper.GetSearchResult(brandId, brands, brand => Task.FromResult(brand.Name));
         return Json(gridModel);
     }
 }
