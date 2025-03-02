7667b8e7feae34dabd2ce94da04ccb58c2462f52
KrzysztofPajak <krzysiek@grandnode.com>
2025-03-02 19:16:58 +0100

    Remove this unnecessary check for null.

--- src/Web/Grand.Web.Vendor/Services/MerchandiseReturnViewModelService.cs
+++ src/Web/Grand.Web.Vendor/Services/MerchandiseReturnViewModelService.cs
@@ -196,9 +196,9 @@ public class MerchandiseReturnViewModelService : IMerchandiseReturnViewModelServ
             var orderItem = order.OrderItems.FirstOrDefault(x => x.Id == item.OrderItemId);
             ArgumentNullException.ThrowIfNull(orderItem);
             items.Add(new MerchandiseReturnModel.MerchandiseReturnItemModel {
-                ProductId = orderItem?.ProductId,
-                ProductName = (await _productService.GetProductByIdIncludeArch(orderItem?.ProductId)).Name,
-                ProductSku = orderItem?.Sku,
+                ProductId = orderItem.ProductId,
+                ProductName = (await _productService.GetProductByIdIncludeArch(orderItem.ProductId)).Name,
+                ProductSku = orderItem.Sku,
                 Quantity = item.Quantity,
                 UnitPrice = _priceFormatter.FormatPrice(orderItem!.UnitPriceInclTax),
                 ReasonForReturn = item.ReasonForReturn,
