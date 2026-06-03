fbf417c604d6d1a06c9c72b16b9c37d01ea72f31
KateB-code <katia.bieliatko@mail.ru>
2026-06-04 00:35:14 +0300

    refactor: remove unused mock scaffolding from BrandingSettingsTests

--- src/Tests/Grand.Business.Common.Tests/Services/Configuration/BrandingSettingsTests.cs
+++ src/Tests/Grand.Business.Common.Tests/Services/Configuration/BrandingSettingsTests.cs
@@ -1,29 +1,12 @@
 using Grand.Business.Core.Interfaces.Common.Configuration;
-using Grand.Data;
-using Grand.Domain.Configuration;
 using Grand.Domain.Stores;
-using Grand.Infrastructure.Caching;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 
 namespace Grand.Business.Common.Tests.Services.Configuration;
 
 [TestClass]
 public class BrandingSettingsTests
 {
-    private Mock<ICacheBase> _cacheMock;
-    private Mock<IRepository<Setting>> _repositoryMock;
-
-    [TestInitialize]
-    public void Init()
-    {
-        _cacheMock = new Mock<ICacheBase>();
-        _repositoryMock = new Mock<IRepository<Setting>>();
-        _cacheMock
-            .Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<Func<Task<IList<Setting>>>>()))
-            .ReturnsAsync(new List<Setting>());
-    }
-
     [TestMethod]
     public void BrandingSettings_DefaultInstance_HasNullColorProperties()
     {
